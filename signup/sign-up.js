4930175fe741047675fe4bbb5b93c8bc387e8c2b
JabenJu <89491072+JabenJu@users.noreply.github.com>
2021-11-09 08:11:58 -0500

    Fixed links and changed password to email

--- signup/sign-up.js
+++ signup/sign-up.js
@@ -1,20 +1,21 @@
 let username = null
-let password = null
-let loginPath = "login.html"
+let email = null
+let loginPath = "../login/login.html"
+let emailRegex = new RegExp("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$")
 
 function signUp()
 {
     username = $("#username").val()
     console.log()
-    password = $("#password").val()
-    if( isNaN(username.length) || isNaN(password.length))
+    email = $("#email").val()
+    if( isNaN(username.length) || isNaN(email.length) || emailRegex.test(email) )
     {
-        alert("Please enter a valid username and password")
+        alert("Please enter a valid username and email")
     }
     else
     {
         localStorage.setItem("username", username)
-        localStorage.setItem("password", password)
+        localStorage.setItem("email", email)
         location = loginPath
     }
 }
