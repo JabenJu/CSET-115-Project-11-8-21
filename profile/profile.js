70ab83ee3650644b9e21505bbe10d3ed72bff952
willdevny <willdevny12@gmail.com>
2021-11-10 07:57:03 -0500

    not needed, but nice to have

--- profile/profile.js
+++ profile/profile.js
@@ -1,9 +1,22 @@
-let user = localStorage.getItem("username")
-let TTTwins = localStorage.getItem("TTTscore")
-let RPSwins = localStorage.getItem("RPSscore")
+//let user = localStorage.getItem("username")
+//let TTTwins = localStorage.getItem("TTTscore")
+//let RPSwins = localStorage.getItem("RPSscore")
+
+let user = "user"
+let TTTwins = 0
+let RPSwins = 0
 
 document.addEventListener("DOMContentLoaded", function() {
     $("#user").html(user)
+    $("table").hover(function(){
+        $("#TTTwins").html(TTTwins)
+        $("#RPSwins").html(RPSwins)
+    });
+    //$("#TTTwins").html(TTTwins)
+    //$("#RPSwins").html(RPSwins)
+});
+
+$("table").hover(function(){
     $("#TTTwins").html(TTTwins)
     $("#RPSwins").html(RPSwins)
-  });
+});
